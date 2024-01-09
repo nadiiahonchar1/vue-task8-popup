a758ffff2b53559eb246fa605b55d6d64c8e5f4f
nadiiahonchar1 <nadiiahonchar1@gmail.com>
2024-01-09 16:45:40 +0200

    added modal

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,25 @@
 <template>
-  <div></div>
+  <div class="container">
+    <div class="card">
+      <h3>A modal window will open here</h3>
+      <button class="btn" @click="modal = true">Open modal</button>
+      <teleport to="#modal">
+        <app-modal v-if="modal" @close="modal = false"></app-modal>
+      </teleport>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
+import AppModal from "./components/AppModal.vue";
 export default {
   name: "App",
-  components: {},
+  components: { AppModal },
+  data() {
+    return {
+      modal: false,
+    };
+  },
 };
 </script>
 
@@ -19,5 +31,38 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+body {
+  background-color: rgb(255 255 0 / 30%);
+}
+
+.container {
+  margin: 0 auto;
+  width: 50vw;
+}
+
+.card {
+  border: 1px solid rgb(87, 87, 199);
+  border-radius: 10px;
+  margin-bottom: 5px;
+  padding: 5px 20px;
+}
+
+.btn {
+  display: inline-block;
+  min-width: 100px;
+  padding: 5px;
+  border-radius: 20px;
+  background-color: transparent;
+  border: 1px solid #2756a8ad;
+  color: #2756a8ad;
+  cursor: pointer;
+}
+
+.btn:hover {
+  border: 1px solid #2756a8ad;
+  background-color: #2756a8ad;
+  color: white;
+  border-color: #022950;
 }
 </style>
---
+++ src/components/AppModal.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="modal-backdrop" @click="$emit('close')">
+    <div class="modal">
+      <h3>This is the name of the modal</h3>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa quis
+        excepturi architecto tempore ipsam dolore fugiat voluptatibus aperiam!
+        Quidem, corporis.
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  emits: ["close"],
+};
+</script>
+
+<style scoped>
+.modal {
+  position: fixed;
+  top: 60px;
+  width: 600px;
+  padding: 1rem;
+  background: #fff;
+  z-index: 1000;
+  left: 50%;
+  border-radius: 10px;
+  transform: translateX(-50%);
+  box-shadow: 2px 3px 10px rgba(0, 0, 0, 0.2);
+}
+
+.modal-backdrop {
+  position: fixed;
+  top: 0;
+  right: 0;
+  left: 0;
+  bottom: 0;
+  background: rgba(0, 0, 0, 0.35);
+}
+</style>
